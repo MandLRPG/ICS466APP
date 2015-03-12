c261c32e348c4e0a0d0d19daf81da81cdd4421c3
MandLRPG <knaka@hawaii.edu>
2015-03-11 16:47:42 -1000

    Added functionality that shows the users latitude and longitudinal location

--- app/src/main/java/com/example/uninstall/ics466app/MainActivity.java
+++ app/src/main/java/com/example/uninstall/ics466app/MainActivity.java
@@ -1,19 +1,77 @@
 package com.example.uninstall.ics466app;
 
+import android.location.Location;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.api.*;
+import android.support.v4.app.FragmentActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+    private GoogleApiClient mGoogleApiClient;
+    TextView mLatitudeText, mLongitudeText;
+    Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buildGoogleApiClient();
+        mLatitudeText = (TextView) findViewById(R.id.mLatitudeText);
+        mLongitudeText = (TextView) findViewById(R.id.mLongitudeText);
     }
 
+    protected synchronized void buildGoogleApiClient(){
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(true) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint){
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mLastLocation != null){
+            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        //stub
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result){
+        //stub
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
