52c7e2960a3850a43d8dbf43526e7770ad6745d3
MandLRPG <knaka@hawaii.edu>
2015-03-20 02:48:01 -1000

    Added a few fragment that has 10 predefined buttons which can possibly be modified by the program. Added said fragment to the account page.

---
+++ app/src/main/java/com/example/uninstall/ics466app/BookSearchFragment.java
@@ -0,0 +1,22 @@
+package com.example.uninstall.ics466app;
+
+/**
+ * Created by uninstall on 3/20/15.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BookSearchFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.book_search_fragment, container, false);
+        return view;
+    }
+
+}
