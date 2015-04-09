5f1c4b8bac988a323c6aaecb06da63790414dec8
MandLRPG <knaka@hawaii.edu>
2015-04-08 19:34:36 -1000

    Added functionality so that the pages work like they should regardless of which orientation the phone is in.

--- app/src/main/java/com/example/uninstall/ics466app/NewPostActivity.java
+++ app/src/main/java/com/example/uninstall/ics466app/NewPostActivity.java
@@ -1,72 +1,16 @@
 package com.example.uninstall.ics466app;
 
-import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
-
-
-public class NewPostActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
-
-    private GoogleApiClient mGoogleApiClient;
-    private TextView mLatitudeText, mLongitudeText;
-    private Location mLastLocation;
+public class NewPostActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
-
-        buildGoogleApiClient();
-        mLatitudeText = (TextView) findViewById(R.id.mLatitudeText);
-        mLongitudeText = (TextView) findViewById(R.id.mLongitudeText);
-    }
-
-    protected synchronized void buildGoogleApiClient(){
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        if(true) {
-            mGoogleApiClient.connect();
-        }
-    }
-
-    @Override
-    protected void onStop(){
-        mGoogleApiClient.disconnect();
-        super.onStop();
-    }
-
-    @Override
-    public void onConnected(Bundle connectionHint){
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if(mLastLocation != null){
-            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int cause) {
-        //stub
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result){
-        //stub
     }
 
     @Override
